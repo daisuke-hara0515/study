3a84a2607a9dda52d46cfa8f0caa9af60131eaff
原大輔 <>
2019-01-03 15:23:58 +0900

    5-5まで復習

--- chapter5.rb
+++ chapter5.rb
@@ -1,3 +1,36 @@
 puts "5-1"
 p ["コーヒー","カフェラテ"].size
 
+p [1,2,3,4,5].sum
+
+puts "5-2"
+
+p ["モカ","カフェラテ","モカ"].uniq
+
+array = [1,2,3]
+p array.clear
+
+puts "5-3"
+
+p ["カフェラテ","モカ","カプチーノ"].sample
+
+omikuji = ["大吉","中吉","末吉","凶"]
+p omikuji.sample
+
+puts "5-4"
+
+p [100,50,300].sort
+
+p [100,50,300].sort.reverse
+
+p "cba".reverse
+
+puts "5-5"
+
+p ["100","50","300"].join(",")
+
+p "100,50,300".split(",")
+
+puts "5-6"
+
+
