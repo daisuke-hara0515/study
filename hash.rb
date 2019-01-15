305c9653456656dd33928b3f2586035666f8f93b
原大輔 <>
2019-01-15 21:08:23 +0900

    ハッシュに関する練習問題確認

--- hash.rb
+++ hash.rb
@@ -1,1 +1,12 @@
-p ({:coffee => 300, :cake => 400} 
+hash = {}
+hash[:カフェラテ] = {}
+hash[:カフェラテ][:モカ] = 100
+
+p hash.keys
+
+
+hash = {}
+hash[:カフェラテ] = "カフェラテ"
+hash["カフェラテ"] = "カフェラテ"
+hash.clear[:カフェラテ]
+p hash
