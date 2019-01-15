c452f3ad48aa51d3409db2d7ba6f99879a38431d
原大輔 <>
2019-01-16 00:13:48 +0900

    8-3まで解答

--- chapter8.rb
+++ chapter8.rb
@@ -1,4 +1,34 @@
 puts "8-1"
 
+p ({:coffee => 300, :caffe_latte => 400}).class
+
+p Hash.new
+
+puts "8-2"
+
+class Caffe_latte
+end
+
+caffe_latte = Caffe_latte.new
+p caffe_latte.class
+
+puts "8-3"
+
+class Item
+    def name
+        "チーズケーキ"
+    end
+end
+
+item = Item.new
+p item.name
+
+puts "8-4"
+
+
+
+
+
+
 
 
