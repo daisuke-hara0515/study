45af949f9f255d0cee418e3f3bcc0c00375a8dbb
daisuke-hara0515 <wildlife402@gmail.com>
2019-02-08 09:34:37 +0900

    ミニツク問題5まで進む

---
+++ test_silver.rb
@@ -0,0 +1,16 @@
+#a = [1,2,3]
+#b = [4,5,6]
+#p a.zip(b)[0]
+
+p [ARGV[0],ARGV[2]]
+
+p (1...5).to_a.map {|i| i * 10}
+
+n = 10
+n.times do |i|
+  puts i + n
+end
+
+s = 0xBacFace
+  s += 1
+  puts s
